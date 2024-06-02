0b4e796d8e4ed17860d6d6d61a52fc613f2a0dbf
Dimitar <lazarovd12@gmail.com>
2024-06-02 23:59:40 +0300

    Basic setup and direction mapping.

--- 2048.cpp
+++ 2048.cpp
@@ -1,20 +1,54 @@
-// 2048.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+int board[4][4];
+int dirLine[] = { 1,0, - 1,0 };
+int dirColumn[] = { 1,0,-1,0 };
+
+void newGame() {
+    for (int i = 0; i < 4; ++i)
+        for (int j = 0; j < 4; ++j)
+            board[i][j] = 0;
+}
+
+void printUI() {
+    for (int i = 0; i < 4; ++i) {
+
+        for (int j = 0; j < 4; ++j)
+            if (board[i][j] == 0)
+                std::cout << " ";
+            else
+                std::cout << board[i][j];
+    std::cout << "\n";
+    }
+    std::cout << "n: new game, w: up, s: down, d: right, a: left, q: quit\n";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char commandToDir[128]; //0-128 ascii chars
+    commandToDir['s'] = 0;
+    commandToDir['d'] = 1;
+    commandToDir['w'] = 2;
+    commandToDir['a'] = 3;
+    newGame();
+    while (true)
+    {
+        printUI();
+        char command;
+        std::cin >> command;
+
+        if (command == 'n') {
+            newGame(); 
+        }
+        else if (command == 'q') {
+            break;
+        }
+        else {
+            int currentDirection = commandToDir[command];
+            std::cout << currentDirection;
+            //applyMove(currentDirection); to do
+        }
+    }
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
